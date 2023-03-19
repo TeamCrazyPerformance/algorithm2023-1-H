7f9cdbcf610f3a2a631483f81e7005a7724338ef
Hyeongjin Lee <lhjin0j@gmail.com>
2023-03-19 22:58:05 +0900

    1주차 필수 완료

---
+++ 이형진/1주차/특정한최단경로/build/CMakeFiles/output.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/output.dir/main.cpp.o"
+  "CMakeFiles/output.dir/main.cpp.o.d"
+  "output"
+  "output.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
