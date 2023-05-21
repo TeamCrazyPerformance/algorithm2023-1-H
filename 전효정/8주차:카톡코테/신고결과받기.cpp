e494bfd0cc8c38d81bcad5eedc122bc8b44c9093
Hyojeong Jun <junnie082@gmail.com>
2023-05-22 00:12:08 +0900

    선고결과받기.cpp

---
+++ 전효정/8주차:카톡코테/신고결과받기.cpp
@@ -0,0 +1,29 @@
+// https://word.tistory.com/24
+
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    // 1. 
+    const int n = id_list.size();
+    map<string, int> Conv;
+    for (int i = 0; i < n; i++) Conv[id_list[i]] = i; 
+
+    // 2.
+    vector<pair<int, int> > v; 
+    sort(report.begin(), report.end());
+    report.erase(unique(report.begin(), report.end()), report.end());
+
+    for (const auto& s: report) {
+        stringstream in(s); 
+        string a, b; in >> a >> b; 
+        v.push_back({ Conv[a], Conv[b] });
+    }
+
+    // 3.
+    vector<int> cnt(n), ret(n);
+    for (const auto& [a,b] : v) cnt[b]++;
+    for (const auto& [a,b] : v) if (cnt[b] >= k) ret[a]++;
+    return ret;
+}
